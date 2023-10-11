3600f7927aef84bf902381062ed425f76631af60
SonitBahl <sonitbahl@gmail.com>
2023-10-11 17:48:45 +0530

    added app based interface for client

--- client.py
+++ client.py
@@ -1,49 +1,60 @@
-# Client 
-# Import socket module
+# Client
 
+# Import socket module for network communication
+# Import tkinter module for creating a graphical user interface
 import socket
- 
-# In this Line we define our local host
-# address with port number
+import tkinter as tk
 
+# Function to send a request to the server
+def send_request():
+    inp = input_field.get()
+    if inp.lower() == "over":
+        client.send(inp.encode())
+        client.close()
+        root.quit()
+    else:
+        client.send(inp.encode())
+        answer = client.recv(1024)
+        answer_label.config(text="Answer is " + answer.decode())
+
+# Function to clear the input field
+def clear_input():
+    input_field.delete(0, tk.END)
+
+# Create the main tkinter window
+root = tk.Tk()
+root.title("Simple Calculator")
+
+# Set the background color of the main window to black
+root.configure(bg='black')
+
+# Create a label for user instructions, set the background to black, and text color to light blue
+input_label = tk.Label(root, text="Enter the operation (operand operator operand)", bg='black', fg='light blue')
+input_label.pack()
+
+# Create an input field for user input, set the background to black, and text color to light blue
+input_field = tk.Entry(root, bg='black', fg='light blue')
+input_field.pack()
+
+# Create a "Submit" button, set the background to black, and text color to light blue
+submit_button = tk.Button(root, text="Submit", command=send_request, bg='black', fg='light blue')
+submit_button.pack()
+
+# Create a "Clear" button, set the background to black, and text color to light blue
+clear_button = tk.Button(root, text="Clear", command=clear_input, bg='black', fg='light blue')
+clear_button.pack()
+
+# Create a label to display the answer, set the background to black, and text color to light blue
+answer_label = tk.Label(root, text="", bg='black', fg='light blue')
+answer_label.pack()
+
+# Define the server's address and port
 SERVER = "127.0.0.1"
-
 PORT = 8080
-# Making a socket instance
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-# connect to the server
+# Create a socket and connect to the server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
-# Running a infinite loop
 
-while True:
-
-    print("Example : 4 + 5")
-
-    # here we get the input from the user
-
-    inp = input("Enter the operation in \
-        the form opreand operator oprenad: ")
-
-    # If user wants to terminate
-    # the server connection he can type Over
-
-    if inp == "Over":
-
-        break
-
-    # Here we send the user input
-    # to server socket by send Method
-
-    client.send(inp.encode())
- 
-
-    # Here we received output from the server socket
-
-    answer = client.recv(1024)
-
-    print("Answer is "+answer.decode())
-
-    print("Type 'Over' to terminate")
- 
-client.close()
+# Start the tkinter main event loop
+root.mainloop()
